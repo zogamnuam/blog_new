e1090033aa549501a37d0211c4025a93223c4dee
zogamnuam <khaipi@gmail.com>
2015-05-14 18:51:02 +0000

    change username

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  http_basic_authenticate_with name: "mh", password: "secret",
+  http_basic_authenticate_with name: "thomas", password: "secret",
   except: [:index, :show]
   
   def new
